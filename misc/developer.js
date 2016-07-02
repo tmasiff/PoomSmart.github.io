57b887aa191efc5e39d1c7080654f6ebcd3f3474
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-02 10:51:34 +0700

    Updated.

---
+++ misc/developer.js
@@ -0,0 +1,8 @@
+document.write('\
+	<label>Information</label>\
+        <fieldset>\
+            <a href="https://twitter.com/PoomSmart"><img class="icon" src="../../assets/icons/twitter-256-black.png"><div><div><label><p>Developer\'s Twitter</p></label></div></div></a>\
+            <a href="https://github.com/PoomSmart"><img class="icon" src="../../assets/icons/github-256-black.png"><div><div><label><p>Developer\'s GitHub</p></label></div></div></a>\
+            <a href="https://PoomSmart.github.io/repo"><div><div><label><p>&#x1f4b0 Donate to this developer</p></label></div></div></a>\
+        </fieldset>\
+');
