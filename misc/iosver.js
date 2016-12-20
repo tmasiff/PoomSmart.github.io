80f7bc8c16a92bbcb24039665eafae98df457552
Thatchapon Unprasert <poomst@hotmail.com>
2016-12-20 16:26:44 +0700

    Actual fix.

--- misc/iosver.js
+++ misc/iosver.js
@@ -5,57 +5,20 @@ const VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s";
 
 (function(document) {
 	"use strict";
+	
+	function toNum(bits) {
+		return 10000 * bits[0] + 100 * (bits[1] ? bits[1] : 0) + (bits[2] ? bits[2] : 0);
+	}
 
 	function parseVersionString(version) {
 		var bits = version.split(".");
-		return [ bits[0], bits[1] ? bits[1] : 0, bits[2] ? bits[2] : 0 ];
+		return toNum(bits);
 	}
 
-	function compareVersions(v1, v2, options) {
-		var lexicographical = options && options.lexicographical,
-        zeroExtend = options && options.zeroExtend,
-        v1parts = v1.split('.'),
-        v2parts = v2.split('.');
-
-		function isValidPart(x) {
-			return (lexicographical ? /^\d+[A-Za-z]*$/ : /^\d+$/).test(x);
-		}
-
-		if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
-			return NaN;
-		}
-
-		if (zeroExtend) {
-			while (v1parts.length < v2parts.length) v1parts.push("0");
-			while (v2parts.length < v1parts.length) v2parts.push("0");
-		}
-
-		if (!lexicographical) {
-			v1parts = v1parts.map(Number);
-			v2parts = v2parts.map(Number);
-		}
-
-		for (var i = 0; i < v1parts.length; ++i) {
-			if (v2parts.length == i) {
-				return 1;
-			}
-
-			if (v1parts[i] == v2parts[i]) {
-				continue;
-			}
-			else if (v1parts[i] > v2parts[i]) {
-				return 1;
-			}
-			else {
-				return -1;
-			}
-		}
-
-		if (v1parts.length != v2parts.length) {
-			return -1;
-		}
-
-		return 0;
+	function compareVersions(one, two) {
+		console.log(one);
+		var two_ = toNum(two);
+		return one != two_ ? (one > two_ ? 1 : -1) : 0;
 	}
 
 	var prerequisite = document.querySelector(".prerequisite"),
@@ -64,10 +27,13 @@ const VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s";
 	if (!prerequisite || !version) {
 		return;
 	}
+	
+	console.log(version);
 
 	var osVersion = [ version[2], version[3], version[4] ? version[5] : 0 ],
 
 		osString = osVersion[0] + "." + osVersion[1] + (osVersion[2] && osVersion[2] != 0 ? "." + osVersion[2] : ""),
+		
 		minString = prerequisite.dataset.minIos,
 		maxString = prerequisite.dataset.maxIos,
 
@@ -90,7 +56,6 @@ const VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s";
 		isBad = true;
 	}
 
-//	prerequisite.querySelector("p").textContent = message;
     prerequisite.querySelector("p").innerHTML = message;
 
 	if (isBad) {
